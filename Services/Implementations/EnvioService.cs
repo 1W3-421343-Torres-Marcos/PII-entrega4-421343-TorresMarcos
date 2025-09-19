9773709be0cf1cabe0c10b43a5fd9e1326be36d5
1w3-421343-Torres-Marcos <421343@tecnicatura.frc.utn.edu.ar>
2025-09-19 09:40:48 -0300

    agregada la implementacion que no se subio del service

--- Services/Implementations/EnvioService.cs
+++ Services/Implementations/EnvioService.cs
@@ -1,6 +1,48 @@
-﻿namespace Envios_comercio.Services.Implementations
+﻿using Envios_comercio.Repositories.Dtos;
+using Envios_comercio.Services.Interfaces;
+
+namespace Envios_comercio.Services.Implementations
 {
-    public class EnvioService
+    public class EnvioService : IEnvioService
     {
+        public bool CreateProduct(CreateEnvioDto envioDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteProduct(int codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EnvioDto> GetActivos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EnvioDto> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EnvioDto? GetProductById(int codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveProduct(EnvioDto product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EnvioDto> SearchByCostumer(string dni)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateProduct(UpdateEnvioDto envioDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
